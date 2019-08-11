499d2c5a95e635d90cf85986d79e4b26b41524e2
Joao S O Bueno <gwidion@gmail.com>
2019-08-11 12:13:17 -0300

    Threading test

--- tests/test_async.py
+++ tests/test_async.py
@@ -27,3 +27,36 @@ def test_context_local_vars_work_for_async():
         assert ctx.value == -1
 
     manager()
+
+
+
+def test_context_local_async_reflect_changes_made_downstream():
+
+
+    ctx = ContextLocal()
+
+    results = set()
+
+    @ctx.context
+    async def worker(value):
+        ctx.value = value
+        results.add(ctx.value)
+        await second_stage_worker()
+        assert ctx.value == value + 1
+
+    async def second_stage_worker():
+        await asyncio.sleep((10 - ctx.value) * 0.01)
+        ctx.value += 1
+        results.add(ctx.value)
+
+    @ctx.context
+    def manager():
+        ctx.value = -1
+        tasks = asyncio.gather(*(worker(i) for i in range(0, 10, 2)))
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(tasks)
+        print(results)
+        assert all(i in results for i in range(10))
+        assert ctx.value == -1
+
+    manager()
---
+++ tests/test_thread.py
@@ -0,0 +1,34 @@
+import threading
+import time
+from collections import deque
+
+from extracontext import ContextLocal
+
+consume = deque(maxlen=0).extend
+
+
+def test_context_local_vars_work_for_threads():
+
+
+    ctx = ContextLocal()
+
+    results = set()
+
+    @ctx.context
+    def worker(value):
+        ctx.value = value
+        time.sleep((10 - value) * 0.01)
+        assert value == ctx.value
+        results.add(ctx.value)
+
+    @ctx.context
+    def manager():
+        ctx.value = -1
+        tasks = [threading.Thread(target=worker, args=(i,)) for i in range(10)]
+        consume(t.start() for t in tasks)
+        consume(t.join() for t in tasks)
+        print(results)
+        assert all(i in results for i in range(10))
+        assert ctx.value == -1
+
+    manager()
