7cd3987b24e5c09f15bd5e0eb78026ceb888bae8
Joao S O Bueno <gwidion@gmail.com>
2024-11-13 09:42:46 -0300

    Added tdd test

--- extracontext/__init__.py
+++ extracontext/__init__.py
@@ -2,6 +2,7 @@ from .base import ContextLocal
 from .contextlocal import PyContextLocal, ContextError
 from .mapping import ContextMap
 from .contextlocal_native import NativeContextLocal
+from .executor import ContextPreservingExecutor
 
 __version__ = "1.0.0+0"
 
--- tests/test_executor.py
+++ tests/test_executor.py
@@ -1,17 +1,54 @@
+import asyncio
 import threading
 import time
 from collections import deque
+
+from contextvars import ContextVar, copy_context
 
 import pytest
 
 from extracontext import ContextPreservingExecutor
 
 
-consume = deque(maxlen=0).extend
 
 
-def test_executor_preserces_context:
+def test_executor_preserves_context():
+    # test executed using ordinary contexts
 
     ctx = ContextPreservingExecutor(1)
-    ...
-    # [WIP]
+
+    myvar = None
+
+    all_ok, message = False, ""
+
+    def stage_1():
+        nonlocal myvar
+        myvar = ContextVar("myvar", default=5)
+        asyncio.run(stage_2())
+        # assert myvar.get() == 5  # defautl contextvar behavior!
+
+    async def stage_2():
+        nonlocal all_ok, message  # myvar
+
+        myvar.set(23)
+        loop = asyncio.get_running_loop()
+        task1 = loop.run_in_executor(ctx, stage_3)
+        task2 = asyncio.sleep(0.05)
+        await asyncio.gather(task1, task2)
+        if all_ok:
+            all_ok = myvar.get() == 23
+            if not all_ok:
+                message = "Context var not reset to in-task value after thread call"
+
+    def stage_3():
+        nonlocal all_ok, message
+        all_ok = (x:=myvar.get() == 23)
+        print("\n****", "\n", x, "\n*****\n")
+        if not all_ok:
+            message = f"Context var set to {myvar.get()} in thread worker. Expecting 23!"
+        myvar.set(42)
+
+
+    copy_context().run(stage_1)
+
+    assert all_ok, message
