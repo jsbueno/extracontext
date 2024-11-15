0863049764feb687ba8ffa3af54b5de92d1aea20
Joao S O Bueno <gwidion@gmail.com>
2024-11-15 16:09:02 -0300

    [WIP]

--- extracontext/base.py
+++ extracontext/base.py
@@ -5,14 +5,14 @@ from typing import cast, Any, Optional
 class ContextLocal:
     _backend_registry: dict[str, type["ContextLocal"]] = {}
 
-    def __new__(cls, *args: Any, backend: Optional[str]=None, **kwargs: Any) -> Self:
+    def __new__(cls, *args: Any, backend: Optional[str]=None, **kwargs: Any) -> "ContextLocal":
         if backend is None:
             backend = getattr(cls, "_backend_key", "native")
         cls = cls._backend_registry[backend]
         ## Do not forward arguments to object.__new__
         if len(ContextLocal.__mro__) == 2:
             args, kwargs = (), {}
-        return cast(Self, super().__new__(cls, *args, **kwargs))
+        return cast("ContextLocal", super().__new__(cls, *args, **kwargs))
 
     def __init__(self, *, backend: Optional[str]=None):
         pass
